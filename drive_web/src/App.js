96f415aaa6ce83396c5c10837dd6f7742c1127df
Martin Blume <martin.blume@senacor.com>
2016-12-15 15:07:09 +0100

    drive_server angebunden

--- drive_web/src/App.js
+++ drive_web/src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Key from './Key.jsx';
+import {DirectionCommand, StopCommand} from './Command'
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.down = this.down.bind(this)
+    this.up = this.up.bind(this)
+  }
+
+  move(direction){
+    new DirectionCommand(direction).move()
+  }
+
+  stop(event){
+    new StopCommand().stop()
+  }
+
+  down(event) {
+    if(event.key != this.buttonPressed){
+      this.buttonPressed = event.key
+      this.move(this._translateDirection(event.key))
+    }
+  }
+
+  up(event) {
+    if(event.key == this.buttonPressed){
+      this.stop()
+    }
+  }
+
+  _translateDirection(direction){
+    switch (direction) {
+      case 'ArrowUp' : return 'up'
+      case 'ArrowDown' : return 'down'
+      case 'ArrowLeft' : return 'left'
+      case 'ArrowRight' : return 'right'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div tabIndex="0" onKeyDown={this.down} onKeyUp={this.up}>
+        <div className="App" >
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Drive the slowest car in the world!</h2>
+          </div>
+          <div className="App-intro container">
+            <Key direction="up"/>
+          </div>
+          <div className="App-intro container">
+            <Key direction="left"/>
+            <Key direction="down"/>
+            <Key direction="right"/>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ drive_web/src/Key.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import {StopCommand, DirectionCommand} from './Command'
+
+class Key extends Component{
+
+	constructor() {
+    	super()
+    	this.state = {
+      		pressed: false,
+    	}
+
+    	this.start = this.start.bind(this)
+    	this.stop = this.stop.bind(this)
+  	}
+
+  	start() {
+    	new DirectionCommand(this.props.direction).move()
+  	}
+
+	stop() {
+		new StopCommand().stop()
+	}
+
+	render() {
+		return (
+          <div><img src={this._image()} alt={this.props.direction} class="img-responsive" onMouseDown={this.start} onMouseUp={this.stop}/></div>
+    	);
+	}
+
+	_image() {
+		return this.props.direction + '.png'
+	}
+
+}
+
+export default Key;
